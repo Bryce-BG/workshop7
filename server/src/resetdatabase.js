730b103ad9d5bc534c452ac38bdd042732275a7e
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-04-15 18:36:41 -0400

    databased

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -107,6 +107,14 @@ var initialData = {
   }
 };
 
+
+
+/**
+ * Adds any desired indexes to the database.
+ */
+function addIndexes(db, cb) {
+  db.collection('feedItems').createIndex({ "contents.contents": "text" }, null, cb);
+}
 /**
  * Resets a collection.
  */
@@ -143,7 +151,7 @@ function resetDatabase(db, cb) {
       // Use myself as a callback.
       resetCollection(db, collection, processNextCollection);
     } else {
-      cb();
+        addIndexes(db, cb);
     }
   }
 
